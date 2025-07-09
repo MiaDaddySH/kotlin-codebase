1ce23cd46a386bc9b6dc5d923894323fb34d5d38
Yuangang Sheng <yuangang.sheng@intive.com>
2025-07-09 11:41:10 +0200

    Initial project structure for Kotlin codebase

---
+++ setup_structure.sh
@@ -0,0 +1,2 @@
+mkdir -p utils extensions android-modules ktor-backend scripts templates
+touch utils/.gitkeep extensions/.gitkeep android-modules/.gitkeep ktor-backend/.gitkeep scripts/.gitkeep templates/.gitkeep
